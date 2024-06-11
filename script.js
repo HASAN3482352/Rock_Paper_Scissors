95c5ce812113863e135f77c4fe8ea7f5fa87452f
Hasan Iqbal Khan <hik77920@gmail.com>
2024-06-12 04:40:29 +0530

    REFACTOR THE CODE TO MAKE THE PADE INTERACTIVE

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+const body = document.querySelector('body');
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+const li2 = document.createElement('li');
+const li3 = document.createElement('li');
+const button1 = document.createElement('button');
+const button2 = document.createElement('button');
+const button3 = document.createElement('button');
+const h1 = document.createElement('h1');
+const div = document.createElement('div');
+button1.textContent='rock';
+button2.textContent='paper';
+button3.textContent='scissor';
+button1.setAttribute('id','rock');
+button2.setAttribute('id','paper');
+button3.setAttribute('id','scissor');
+h1.textContent='Select an option:';
+ul.style.cssText="list-style-type:none;display:flex;";
+li1.appendChild(button1);
+li2.appendChild(button2);
+li3.appendChild(button3);
+ul.appendChild(li1);
+ul.appendChild(li2);
+ul.appendChild(li3);
+body.appendChild(h1);
+body.appendChild(ul);
+
+let humanScore=0,computerScore=0;
+
+function playRound(humanChoice,computerChoice){
+    if(humanChoice.toUpperCase()=="SCISSOR" && computerChoice=="ROCK"){
+        computerScore++;
+        div.textContent=`You lose! ${computerChoice} beats ${humanChoice}-------->humanScore = ${humanScore} and computerScore = ${computerScore}`;
+        body.appendChild(div);
+    }else if(humanChoice.toUpperCase()=="ROCK" && computerChoice=="PAPER"){
+        computerScore++;
+        div.textContent=`You lose! ${computerChoice} beats ${humanChoice}-------->humanScore = ${humanScore} and computerScore = ${computerScore}`;
+        body.appendChild(div);
+    }else if(humanChoice.toUpperCase()=="PAPER" && computerChoice=="SCISSOR"){
+        computerScore++;
+        div.textContent=`You lose! ${computerChoice} beats ${humanChoice}-------->humanScore = ${humanScore} and computerScore = ${computerScore}`;
+        body.appendChild(div);
+    }else if(humanChoice.toUpperCase()=="PAPER" && computerChoice=="ROCK"){
+        humanScore++;
+        div.textContent=`You win! ${humanChoice} beats ${computerChoice}-------->humanScore = ${humanScore} and computerScore = ${computerScore}`;
+        body.appendChild(div);
+    }
+    else if(humanChoice.toUpperCase()=="SCISSOR" && computerChoice=="PAPER"){
+        humanScore++;
+        div.textContent=`You win! ${humanChoice} beats ${computerChoice}-------->humanScore = ${humanScore} and computerScore = ${computerScore}`;
+        body.appendChild(div);
+    }
+    else if(humanChoice.toUpperCase()=="ROCK" && computerChoice=="SCISSOR"){
+        humanScore++;
+        div.textContent=`You win! ${humanChoice} beats ${computerChoice}-------->humanScore = ${humanScore} and computerScore = ${computerScore}`;
+        body.appendChild(div);
+    }else{
+        div.textContent=`No one loses-------->humanScore = ${humanScore} and computerScore = ${computerScore}`;
+        body.appendChild(div);
+    }
+    if(humanScore == 5 || computerScore == 5){
+        if(humanScore>computerScore){
+            div.textContent="You are the Winner!!!";
+            body.appendChild(div);
+        }else{
+            div.textContent="The computer wins this time...";
+            body.appendChild(div);
+        }
+        humanScore=0;
+        computerScore=0
+    }
+}
 function getComputerChoice(){
     const rand = Math.floor(Math.random()*3) + 1;
     let opt = "";
@@ -10,45 +82,21 @@ function getComputerChoice(){
     }
     return opt;
 }
-function getHumanChoice(){
-    let opt = prompt("Rock, Paper or Scissor?");
-    return opt;
-}
-function playGame(){
-    let humanScore=0,computerScore=0;
-    function playRound(humanChoice,computerChoice){
-        if(humanChoice.toUpperCase()=="SCISSOR" && computerChoice=="ROCK"){
-            alert(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }else if(humanChoice.toUpperCase()=="ROCK" && computerChoice=="PAPER"){
-            alert(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }else if(humanChoice.toUpperCase()=="PAPER" && computerChoice=="SCISSOR"){
-            alert(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }else if(humanChoice.toUpperCase()=="PAPER" && computerChoice=="ROCK"){
-            alert(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        }
-        else if(humanChoice.toUpperCase()=="SCISSOR" && computerChoice=="PAPER"){
-            alert(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        }
-        else if(humanChoice.toUpperCase()=="ROCK" && computerChoice=="SCISSOR"){
-            alert(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;
-        }else{
-            alert("No one loses");
-            i--;
-        }
+ul.addEventListener('click',(eve) => {
+    let target = eve.target;
+    let opt;
+    switch(target.id){
+        case 'rock':
+            opt = 'rock';
+            break;
+        case 'paper':
+            opt = 'paper';
+            break;
+        case 'scissor':
+            opt = 'scissor';
+            break;
     }
-    for(i=0;i<5;i++){
-        playRound(getHumanChoice(),getComputerChoice());
-    }
-    if(humanScore>computerScore){
-        alert("You are the Winner!!!");
-    }else{
-        alert("The computer wins this time...");
-    }
-}
-playGame();
+    playRound(opt,getComputerChoice());
+});
+
+
